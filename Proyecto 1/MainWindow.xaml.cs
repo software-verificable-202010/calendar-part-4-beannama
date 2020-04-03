3c672e3a3530c5438e8f62155108e116f3268fa3
Benjamín Naranjo <Ben.Nar.95@gmail.com>
2020-04-03 19:21:24 -0300

    label changing

--- Proyecto 1/MainWindow.xaml.cs
+++ Proyecto 1/MainWindow.xaml.cs
@@ -20,9 +20,79 @@ namespace Proyecto_1
     /// </summary>
     public partial class MainWindow : Window
     {
+        DateTime targeted_date = DateTime.Today;
+        int columns = 7;
+        int rows = 5;
+
+
         public MainWindow()
         {
             InitializeComponent();
+            CalendarCreation();
+            
+        }
+
+        public Label LabelNumberCreation(string content_text)
+        {
+            Label label = new Label();
+            label.HorizontalAlignment = HorizontalAlignment.Left;
+            label.VerticalAlignment = VerticalAlignment.Top;
+            label.Width = 240;
+            label.Height = 30;
+            label.Content = content_text;
+
+            return label;
+        }
+
+        public void AddLabelToCalendar(Label number_label,int targeted_row, int targeted_col)
+        {
+            Grid.SetColumn(number_label, targeted_col);
+            Grid.SetRow(number_label, targeted_row);
+            Calendar_Grid.Children.Add(number_label);
+        }
+
+        public void NumberCreation(int targeted_row, int targeted_col, string content_text)
+        {
+            Label number_label = new Label();
+            number_label = LabelNumberCreation(content_text);
+            AddLabelToCalendar(number_label, targeted_row, targeted_col);
+
+        }
+
+  
+
+        public void TitleChanger()
+        {
+            Month_TextBlock.Text = targeted_date.ToString("MMMM");
+            Year_TextBlock.Text = targeted_date.Year.ToString();
+
+        }
+
+        public void CalendarCreation()
+        {
+            TitleChanger();
+
+            int number_text = 1;
+            for (int targeted_row = 1;targeted_row<=rows; targeted_row++)
+            {
+                for (int targeted_colummn = 0; targeted_colummn <columns; targeted_colummn++)
+                {
+                    NumberCreation(targeted_row, targeted_colummn, number_text.ToString());
+                    number_text++;
+                }
+            }
+        }
+
+        private void Change_Month_Positive(object sender, RoutedEventArgs e)
+        {
+            targeted_date = targeted_date.AddMonths(1);
+            TitleChanger();
+        }
+
+        private void Change_Month_Negative(object sender, RoutedEventArgs e)
+        {
+            targeted_date = targeted_date.AddMonths(-1);
+            TitleChanger();
         }
     }
 }
